5e36bae73a4748eac37499bb6c41355fa6d291dc
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-04 12:56:39 +0300

    Created model for coins

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ mongoose.connect('mongodb+srv://admin:admin@db.bdboune.mongodb.net/?retryWrites=
     useUnifiedTopology: true
 }).then(() => console.log("MongoDB connected"))
   .catch(err => console.error("Connection error:", err));
+
+
+const coinSchema = new mongoose.Schema({
+    name: String,
+    material: String,
+    country: String,
+    year: Number,
+    price: Number
+});
+ 
+const Coin = mongoose.model('Coin', coinSchema);
