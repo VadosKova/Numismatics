c0e1257ed49cd83903e743c8a4cc077fd0b343f3
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-04 13:20:36 +0300

    Added connecting

--- server.js
+++ server.js
@@ -70,4 +70,13 @@ const server = http.createServer(async (req, res) => {
             res.end(JSON.stringify({ error: 'Error' }));
         }
     }
+
+
+});
+
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
