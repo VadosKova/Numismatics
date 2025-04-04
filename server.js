7e04174bd86cd027d8255a7470701dfa1f96939c
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-04 12:52:32 +0300

    Connection to mongodb

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('Hello')
+const http = require('http');
+const mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb+srv://admin:admin@db.bdboune.mongodb.net/?retryWrites=true&w=majority&appName=DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Connection error:", err));
